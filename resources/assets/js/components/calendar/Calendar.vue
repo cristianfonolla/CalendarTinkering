49ee31d1fe0676740c73bd58aca120ace805809b
cristianfonolla <cristian.fonolla@gmail.com>
2017-04-18 17:58:03 +0200

    components addeds

---
+++ resources/assets/js/components/calendar/Calendar.vue
@@ -0,0 +1,27 @@
+<template>
+            <div class="box box-success box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Calendari</h3>
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+
+                    <div id="calendar"></div>
+
+                </div>
+                <!-- /.box-body -->
+            </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component Calendar mounted.')
+        }
+    }
+</script>
---
+++ resources/assets/js/components/calendar/CalendarEvents.vue
@@ -0,0 +1,28 @@
+<template>
+            <div class="box box-success box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Esdeveniments</h3>
+                </div>
+                <div class="box-body">
+                    <div id='external-events'>
+                        <div class='fc-event'>My Event 1</div>
+                        <div class='fc-event'>My Event 2</div>
+                        <div class='fc-event'>My Event 3</div>
+                        <div class='fc-event'>My Event 4</div>
+                        <div class='fc-event'>My Event 5</div>
+                        <p>
+                            <input type='checkbox' id='drop-remove' />
+                            <label for='drop-remove'>remove after drop</label>
+                        </p>
+                    </div>
+                </div>
+            </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component CalendarEvents mounted.')
+        }
+    }
+</script>
